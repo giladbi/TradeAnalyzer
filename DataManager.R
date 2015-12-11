3faf350e87a107929eae3f1552b3e14ae2b4de42
Michael Szczepaniak <mszczepaniak@pobox.com>
2015-12-11 13:13:57 -0700

    remove default values for start and end dates: defer to caller

--- DataManager.R
+++ DataManager.R
@@ -32,9 +32,7 @@ getSP500Tickers <- function() {
 ##
 ## https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22AAPL%22%20and%20startDate%20%3D%20%222015-01-01%22%20and%20endDate%20%3D%20%222015-12-01%22&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
 ## !!!! HAD TO CHANGE https TO http FOR THE CALL TO xmlTreeParse TO WORK !!!!
-getStockQuotes <- function(ticker,
-                           startYYYY_MM_DD = as.character(Sys.Date() - 365),
-                           endYYYY_MM_DD = as.character(Sys.Date()),
+getStockQuotes <- function(ticker, startYYYY_MM_DD, endYYYY_MM_DD,
                            dataFrame=NULL) {
     #install.packages("XML"); install.packages("dplyr")
     library(XML)
