6092778b5af80308950d350e67dfa268cfaf188b
Michael Szczepaniak <mszczepaniak@pobox.com>
2015-12-17 16:00:38 -0700

    make main panel update only after Run Simulation button click

--- tranalyzer/server.R
+++ tranalyzer/server.R
@@ -2,17 +2,19 @@ source("TradingEvaluator.R")
 
 shinyServer(
     function(input, output) {
-        output$oidstock <- renderPrint({input$ticker})
+        output$oidstock <- eventReactive(input$runSimButton, {
+            input$ticker
+        })
         
-        strategyInput <- reactive({
+        dateRangeInput <- eventReactive(input$runSimButton, {
+            paste0("Query starts: ", input$queryDateRange[1],
+                   " | Query ends: ", input$queryDateRange[2])
+        })
+        
+        strategyInput <- eventReactive(input$runSimButton, {
             paste0("Trade Signal: ", input$tradeSignal,
                    " | Fast SMA: ", input$fastSMA,
                    " | Slow Sma: ", input$slowSMA)
-        })
-        
-        dateRangeInput <- reactive({
-            paste0("Query starts: ", input$queryDateRange[1],
-                   " | Query ends: ", input$queryDateRange[2])
         })
         
         output$signalAndParams <- renderPrint({
@@ -34,17 +36,6 @@ shinyServer(
         output$trades <- renderTable({
             runSim()
         })
-        
-#         observeEvent(input$runSimButton, {
-#             output$trades <- renderTable({
-#                 doSimulation(input$ticker,
-#                              as.character(input$queryDateRange[1]),
-#                              as.character(input$queryDateRange[2]),
-#                              signalParms=c(fastDays=input$fastSMA,
-#                                            slowDays=input$slowSMA))
-#             })
-#         })
-        
         
     }
 )
