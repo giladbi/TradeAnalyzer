7a75b42b77a1dc007c0286a268b399bd7cfa6817
Michael Szczepaniak <mszczepaniak@pobox.com>
2015-12-17 15:34:19 -0700

    move dedicated shiny files for clean deployment

---
+++ tranalyzer/TradingEvaluator.R
@@ -0,0 +1,54 @@
+
+
+## Development convenience method. Just changes working dir to where it needs
+## to be for this project.  Add salt to taste...
+setWorking <- function(laptopSys=TRUE) {
+    dirSys <- "C:/data/"  # laptop
+    if(!laptopSys) { dirSys <- "D:/" }  # workstation
+    dirProject <- "dev/TradeAnalyzer"
+    dirWorking <- paste0(dirSys, dirProject)
+    setwd(dirWorking)
+}
+
+addSimColumns <- function(prices, signalGen, sigParms) {
+    source(signalGen)
+    priceData <- configureSignalParms(prices, sigParms)
+    priceData <- appendSignals(priceData)
+    priceData <- getActionsBHS(priceData)
+    source("StrategySimulator.R")
+    priceData <- allInAllOutOaatOnlyLong(priceData)
+    
+    return(priceData)
+}
+
+## Returns a data frame of the results of a simulation for a stock of a given
+## ticker, from startDate to endDate, using the strategy specified by the 
+## signal generator signalGen.
+##
+## ticker - ticker symbol for stock to run signalGen on (e.g. JNJ, AAPL, etc.)
+## signalParms - Vector of named values used by signalGen to generate signals.
+##               Default is a vector with the fast and slow SMA periods used by
+##               SignalGenSmaLongOnlyOpaat.R
+## startDate - Character array of the format: yyyy-mm-dd designating the
+##             starting date of the simulation. Default is 365 day prior today.
+## endDate - Character array of the format: yyyy-mm-dd designating the
+##           ending date of the simulation. Default is today
+## signalGen - R source file used to implement the signal generator to use for
+##             the simulation. Default is SignalGenSmaLongOnlyOpaat.R
+doSimulation <- function(ticker,
+                         startDate = as.character(Sys.Date()-365),
+                         endDate = as.character(Sys.Date()),
+                         signalParms=c(fastDays=9, slowDays=18),
+                         signalGen = "SignalGenSmaLongOnlyOpaat.R",
+                         priceData=NULL) {
+    if(is.null(priceData)) {
+        source("DataManager.R")
+        priceData <- getStockQuotes(ticker, startDate, endDate)
+    }
+    
+    # next line sources StrategySimulator.R for addSimColumns & getNetTable
+    priceData <- addSimColumns(priceData, signalGen, signalParms)
+    
+    return(getNetTable(priceData))
+}
+
---
+++ tranalyzer/ui.R
@@ -0,0 +1,59 @@
+# Get all 30 DJIA symbols to populate the Company select box
+dow30Url <- "http://raw.githubusercontent.com/MichaelSzczepaniak/TradeAnalyzer/master/tranalyzer/data/dow30.csv"
+dow30Stocks <- read.csv(dow30Url, stringsAsFactors = FALSE)
+
+# Get 4 ticker symbols for demo
+demo4TickersUrl <- "https://raw.githubusercontent.com/MichaelSzczepaniak/TradeAnalyzer/master/tranalyzer/data/demo4Tickers.csv"
+demoStocks <- read.csv(demo4TickersUrl, stringsAsFactors = FALSE)
+
+# tickers <- dowStocks$ticker
+tickers <- demoStocks$ticker
+# names(tickers) <- dowStocks$company_name
+names(tickers) <- demoStocks$company_name
+stockList <- as.list(tickers)
+# Get the trading signals to populate Trade Signal select box
+tradeSignalsUrl <- "http://raw.githubusercontent.com/MichaelSzczepaniak/TradeAnalyzer/master/tranalyzer/data/trade_signals.csv"
+#tradeSignalsUrl <- "./data/trade_signals.csv"
+tradeSignals <- read.csv(tradeSignalsUrl, stringsAsFactors = FALSE)
+tradeSignalList <- as.list((tradeSignals$trade_signal))
+
+## Returns the date that's 10 years ago today in the format:
+## yyyy-mm-dd.
+tenYearsAgoToday <- function() {
+    today <- as.POSIXlt(Sys.Date())
+    today$year <- today$year - 10
+    return(as.character(today))
+}
+
+# Dates to use for demo mode
+demoStartDate <- "2014-12-14"; demoEndDate <- "2015-12-14"
+
+pageWithSidebar(
+    headerPanel("Trade Evaluator"),
+    sidebarPanel(
+        selectInput('ticker', label=h4("Company"),
+                    choices=demoStocks, selected=1
+        ),
+        selectInput("tradeSignal", label=h4("Trade Signal:"),
+            choices=tradeSignalList, selected=1
+        ),
+        numericInput('fastSMA', 'Fast SMA:', 9, min = 2, max = 250, step = 1),
+        numericInput('slowSMA', 'Slow SMA:', 18, min = 3, max = 250, step = 1),
+        dateRangeInput('queryDateRange', label = h3("Quote Date Range:"),
+                       start=demoStartDate, end=demoEndDate,
+                       min=demoStartDate, max=demoEndDate),
+        numericInput('accBalance', 'Starting Account Balance:',
+                     10000, min = 5000, max = 1000000, step = 500),
+        actionButton('runSimButton', 'Run Simulation')
+    ),
+    mainPanel(
+        h4('Company Ticker:'),
+        verbatimTextOutput("oidstock"),
+        h4('Signal & Parameters:'),
+        verbatimTextOutput("signalAndParams"),
+        h4('Query Start & End Dates:'),
+        verbatimTextOutput("oidBothQueryDates"),
+        h4('Trades Using This Strategy:'),
+        tableOutput("trades")
+    )
+)
