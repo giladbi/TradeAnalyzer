45f137ba80664ca7c59c2da19ca4e323c1b2389b
Michael Szczepaniak <mszczepaniak@pobox.com>
2015-12-18 17:27:28 -0700

    change refs to read app/data file for select lists

--- tranalyzer/ui.R
+++ tranalyzer/ui.R
@@ -1,25 +1,25 @@
 # Get all 30 DJIA symbols to populate the Company select box
-dow30Url <- "http://raw.githubusercontent.com/MichaelSzczepaniak/TradeAnalyzer/master/tranalyzer/data/dow30.csv"
-dow30Stocks <- read.csv(dow30Url, stringsAsFactors = FALSE)
+# dow30Url <- "http://raw.githubusercontent.com/MichaelSzczepaniak/TradeAnalyzer/master/tranalyzer/data/dow30.csv"
+# dow30Stocks <- read.csv(dow30Url, stringsAsFactors = FALSE)
 
 # Get 4 ticker symbols for demo
-demo4TickersUrl <- "https://raw.githubusercontent.com/MichaelSzczepaniak/TradeAnalyzer/master/tranalyzer/data/demo4Tickers.csv"
+#demo4TickersUrl <- "http://raw.githubusercontent.com/MichaelSzczepaniak/TradeAnalyzer/master/tranalyzer/data/demo4Tickers.csv"
+demo4TickersUrl <- "./data/demo4Tickers.csv"
 demoStocks <- read.csv(demo4TickersUrl, stringsAsFactors = FALSE)
 
 # Get position management strategies
-#posMgmtStratsUrl <- "https://raw.githubusercontent.com/MichaelSzczepaniak/TradeAnalyzer/master/tranalyzer/data/positionStrats.csv"
+# posMgmtStratsUrl <- "http://raw.githubusercontent.com/MichaelSzczepaniak/TradeAnalyzer/master/tranalyzer/data/positionStrats.csv"
 posMgmtStratsUrl <- "./data/positionStrats.csv"
 posMgmtStrats <- read.csv(posMgmtStratsUrl, stringsAsFactors = FALSE)
 posMgmtStratsList <- as.list((posMgmtStrats$Position_Sizing_Strategy))
 
 # tickers <- dowStocks$ticker
 tickers <- demoStocks$ticker
-# names(tickers) <- dowStocks$company_name
-names(tickers) <- demoStocks$company_name
+names(tickers) <- paste0(demoStocks$ticker, " = ", demoStocks$company_name)
 stockList <- as.list(tickers)
 # Get the trading signals to populate Trade Signal select box
-tradeSignalsUrl <- "http://raw.githubusercontent.com/MichaelSzczepaniak/TradeAnalyzer/master/tranalyzer/data/trade_signals.csv"
-#tradeSignalsUrl <- "./data/trade_signals.csv"
+# tradeSignalsUrl <- "http://raw.githubusercontent.com/MichaelSzczepaniak/TradeAnalyzer/master/tranalyzer/data/trade_signals.csv"
+tradeSignalsUrl <- "./data/trade_signals.csv"
 tradeSignals <- read.csv(tradeSignalsUrl, stringsAsFactors = FALSE)
 tradeSignalList <- as.list((tradeSignals$trade_signal))
 
@@ -40,7 +40,7 @@ pageWithSidebar(
     headerPanel("Trade Evaluator"),
     sidebarPanel(
         selectInput('ticker', label=h4("Company"),
-                    choices=demoStocks, selected=1),
+                    choices=stockList, selected=1),
         selectInput("tradeSignal", label=h4("Trade Signal:"),
             choices=tradeSignalList, selected=1),
         sliderInput("fastSlowSma", h4("Fast & Slow SMA"),
@@ -62,6 +62,7 @@ pageWithSidebar(
         h4('Query Start & End Dates:'),
         verbatimTextOutput("oidBothQueryDates"),
         h4("Trades using this signal and position management:"),
+        h5("(Assumes $10 commission for each buy or sell)"),
         tableOutput("trades"),
         h4('Net Trading Profit/Loss:'),
         verbatimTextOutput("oidTradesNet")
