d84f73653a0ec6ab784fc5e922dce3ebc58e3dc4
Michael Szczepaniak <mszczepaniak@pobox.com>
2015-12-23 14:47:02 -0700

    add all signal points, add sma days in trade signal legend

--- tranalyzer/TradingEvaluator.R
+++ tranalyzer/TradingEvaluator.R
@@ -57,7 +57,7 @@ doSimulation <- function(ticker,
 
 ## Create plot that identifies the trades called out by the signal
 ## shift - fraction amount to shift buy signal point down and sell signal up
-makeTradeSignalsPlot <- function(ticker, startDate,endDate, signalParms,
+makeTradeSignalsPlot <- function(ticker, startDate ,endDate, signalParms,
                                  signalGen, startBalance, shift) {
     source("DataManager.R")
     priceData <- getDemoQuotes(ticker, startDate, endDate) # read repo csv
@@ -65,7 +65,7 @@ makeTradeSignalsPlot <- function(ticker, startDate,endDate, signalParms,
     x <- as.Date(priceData$Date) # x axis values
     plot(x, y=priceData$Close, type="l", lwd=2,
          col='black', xlab="Date", ylab="Price ($ USD)")
-    title(paste0("Completed trades for ", ticker, " using SMA cross-over"))
+    title(paste0("Trade signals for ", ticker, " using SMA cross-over"))
     lines(x, y=priceData$FastSma, col='red')
     lines(x, y=priceData$SlowSma, col='blue')
     # get the sell points
@@ -74,12 +74,14 @@ makeTradeSignalsPlot <- function(ticker, startDate,endDate, signalParms,
     sellSignals <- pmax(sells$FastSma, sells$SlowSma) * (1 + shift)
     points(exitDates, sellSignals, pch=6, cex=1.75, col='red', lwd=2)
     completeCount <- length(sells$Shares)
-    buys <- filter(priceData, Actions=="BUY")[1:completeCount, ]
+    buys <- filter(priceData, Actions=="BUY")
     entryDates <- as.Date(buys$Date)
     buySignals <- pmin(buys$FastSma, buys$SlowSma) * (1 - shift)
     points(entryDates, buySignals, pch=2, cex=1.75, col='green', lwd=2)
+    fastSma <- paste0("Fast SMA ", signalParms["fastDays"])
+    slowSma <- paste0("Slow SMA ", signalParms["slowDays"])
     legend('bottom',
-           c("Close Price", "Fast SMA", "Slow SMA", "Buy Signal", "Sell Signal"),
+           c("Close Price", fastSma, slowSma, "Buy Signal", "Sell Signal"),
            lty=c(1,1,1,0,0), pch=c(NA, NA, NA, 2, 6),
            col=c('black', 'red', 'blue', 'green', 'red'))
 }
@@ -88,7 +90,8 @@ makeTradesResultsHist <- function(ticker, startDate,endDate,
                                   signalParms, signalGen, startBalance) {
     sim <- doSimulation(ticker, startDate, endDate,
                         signalParms, signalGen, startBalance)
-    histTitle <- paste0("Results of ", ticker, " trades using SMA cross-over")
+    histTitle <- paste0("Results of completed ", ticker,
+                        " trades using SMA cross-over")
     hist(sim$ProfitLoss, main=histTitle, xlab="Trade Net ($ USD)",
          ylab="Trade Count", yaxt='n')
     axis(2, at=0:length(sim$ProfitLoss))
